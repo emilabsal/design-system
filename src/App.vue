114b3f931ad5142121a5134bcd294d21c690f757
Liliya Absalyamova <emil-absalyam.off@yandex.ru>
2023-01-01 17:00:45 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>hello</div>
+    <base-button title="Hello" type="primary" name="fire" fill="#fff" />
+  </div>
+</template>
+
+<script setup>
+import BaseButton from "./components/base/BaseButton.vue";
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <button :class="`b-button ${type} ${size} ${reversed ? 'reversed' : ''}`">
+    <base-icon
+      :name="name"
+      v-if="name"
+      :fill="fill"
+      :width="width"
+      :height="height"
+    />
+    {{ title }}
+  </button>
+</template>
+
+<script setup>
+import BaseIcon from "./BaseIcon.vue";
+
+const props = defineProps({
+  title: String,
+  type: {
+    type: String,
+    default: "primary",
+    validator: (value) => {
+      return ["primary", "outline"].includes(value);
+    },
+  },
+  size: {
+    type: String,
+    default: "small",
+    validator: (value) => {
+      return ["small", "medium", "large"].includes(value);
+    },
+  },
+  reversed: {
+    type: Boolean,
+    default: false,
+  },
+  name: String,
+  width: [String, Number],
+  height: [String, Number],
+  fill: String,
+});
+</script>
+
+<style lang="sass" scoped>
+.b-button
+  outline: none
+  border: none
+  background-color: transparent
+  cursor: pointer
+  user-select: none
+  white-space: nowrap
+  display: flex
+  align-items: center
+  gap: 8px
+
+.b-button.primary
+  border-radius: 8px
+  background-color: $primary-700
+  color: #fff
+  margin-left: 20px
+  &:hover
+    background-color: $primary-800
+  &:active
+    box-shadow: 0 0 0 6px $primary-200
+
+.b-button.small
+  height: 34px
+  padding: 0 12px
+</style>
